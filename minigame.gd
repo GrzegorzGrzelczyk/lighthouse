9f8c46609d8e8df4ec857c8c298902843ff65352
GrzegorzGrzelczyk <58474551+GrzegorzGrzelczyk@users.noreply.github.com>
2024-10-10 06:41:03 +0200

    Professional development commit title

--- minigame.gd
+++ minigame.gd
@@ -1,9 +1,10 @@
 extends CanvasLayer
 
 var gamebar := {
-	"warmup_length": 20.0,
-	"play_area_length": 100.0,
+	"warmup_length": 100.0,
+	"play_area_length": 400.0,
 	"total_length": 0.0, # Calculated in _ready
+	"pointer_speed": 100.0, # [px/s]
 	"cards": {
 		"standard": {
 			"width": 10.0
@@ -11,6 +12,19 @@ var gamebar := {
 	}
 }
 
+@onready var pointer: TextureProgressBar = $MinigameMargin/Pointer
+@onready var warmup: NinePatchRect = $MinigameMargin/Warmup
+@onready var playpart: NinePatchRect = $MinigameMargin/Playpart
+
 
 func _ready() -> void:
 	gamebar["total_length"] = gamebar["warmup_length"] + gamebar["play_area_length"]
+	pointer.max_value = gamebar["total_length"]
+	pointer.custom_minimum_size[0] = gamebar["total_length"]
+	warmup.custom_minimum_size[0] = gamebar["warmup_length"]
+	playpart.custom_minimum_size[0] = gamebar["play_area_length"]
+
+
+func _physics_process(delta: float) -> void:
+	pointer.texture_progress_offset[0] += gamebar["pointer_speed"] * delta
+	if pointer.texture_progress_offset[0] >= pointer.max_value: pointer.texture_progress_offset[0] = 0.0
